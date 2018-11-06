1a3137a46bcd8ffff04c9f832f4a4292a6f77f07
prdxTO <itsbradchow@gmail.com>
2018-11-06 17:26:00 -0500

    Removed debug printlns.

---
+++ src/ExitKeyListener.java
@@ -0,0 +1,28 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ExitKeyListener implements KeyListener {
+  @Override
+  public void keyTyped(KeyEvent e) {
+    if(e.getKeyCode() == KeyEvent.VK_NUMPAD0) {
+      Main.toggle = false;
+      System.exit(0);
+    }
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    if(e.getKeyCode() == KeyEvent.VK_NUMPAD0) {
+      Main.toggle = false;
+      System.exit(0);
+    }
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    if(e.getKeyCode() == KeyEvent.VK_NUMPAD0) {
+      Main.toggle = false;
+      System.exit(0);
+    }
+  }
+}
